a5097db39db59c17f6c8644418cdc7581d84cbaa
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2023-09-08 18:24:37 +0700

    Update koneksi.php

--- Tugas 1/koneksi.php
+++ Tugas 1/koneksi.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $database = "data";
 
-// Membuat koneksi
 $conn = new mysqli($host, $username, $password, $database);
 
-// Memeriksa koneksi
 if ($conn->connect_error) {
     die("Koneksi ke database gagal: " . $conn->connect_error);
 }
