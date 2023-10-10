ce7fec7648ebf626ba3f1c6b3885daf57b18f39f
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2023-10-10 17:43:31 +0700

    Create thank_you.php

---
+++ Tugas 2/Secure From Submit/thank_you.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Terima Kasih</title>
+</head>
+<body>
+    <h2>Terima Kasih!</h2>
+    <p>Pesan Anda telah berhasil dikirim. Kami akan segera meresponsnya.</p>
+</body>
+</html>
