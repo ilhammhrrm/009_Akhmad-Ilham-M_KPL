15396d15262e191f11e453b7676dfab4667b69fd
ilhammhrrm <90167055+ilhammhrrm@users.noreply.github.com>
2023-10-10 17:19:02 +0700

    Update register.php

--- Tugas 2/Regristrasi dan login/register.php
+++ Tugas 2/Regristrasi dan login/register.php
@@ -20,6 +20,15 @@ if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['emai
 	// One or more values are empty.
 	exit('Please complete the registration form');
 }
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	exit('Email is not valid!');
+}
+if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
+    exit('Username is not valid!');
+}
+if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+	exit('Password must be between 5 and 20 characters long!');
+}
 // We need to check if the account with that username exists.
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
@@ -32,12 +41,20 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 		echo 'Username exists, please choose another!';
 	} else {
 		// Username doesn't exists, insert new account
-if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, activation_code) VALUES (?, ?, ?, ?)')) {
 	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
 	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+	$uniqid = uniqid();
+	$stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $uniqid);
 	$stmt->execute();
-	echo 'You have successfully registered! You can now login!';
+	$from    = 'noreply@yourdomain.com';
+	$subject = 'Account Activation Required';
+	$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+	// Update the activation variable below
+	$activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $_POST['email'] . '&code=' . $uniqid;
+	$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+	mail($_POST['email'], $subject, $message, $headers);
+	echo 'Please check your email to activate your account!';
 } else {
 	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all three fields.
 	echo 'Could not prepare statement!';
